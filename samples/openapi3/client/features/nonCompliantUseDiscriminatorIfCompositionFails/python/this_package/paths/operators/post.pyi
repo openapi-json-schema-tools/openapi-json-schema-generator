6903d8ab868ddd86bbdcc2f64400c2cd757f8a9c
Justin Black <spacether@users.noreply.github.com>
2022-10-01 13:57:32 -0700

    Adds test_get (#37)
    * Adds test_get
    
    * Samples regenerated

--- samples/openapi3/client/features/nonCompliantUseDiscriminatorIfCompositionFails/python/this_package/paths/operators/post.pyi
+++ samples/openapi3/client/features/nonCompliantUseDiscriminatorIfCompositionFails/python/this_package/paths/operators/post.pyi
@@ -31,6 +31,26 @@ from this_package.model.operator import Operator
 SchemaForRequestBodyApplicationJson = Operator
 
 
+request_body_operator = api_client.RequestBody(
+    content={
+        'application/json': api_client.MediaType(
+            schema=SchemaForRequestBodyApplicationJson),
+    },
+)
+
+
+@dataclass
+class ApiResponseFor200(api_client.ApiResponse):
+    response: urllib3.HTTPResponse
+    body: schemas.Unset = schemas.unset
+    headers: schemas.Unset = schemas.unset
+
+
+_response_for_200 = api_client.OpenApiResponse(
+    response_cls=ApiResponseFor200,
+)
+
+
 class BaseApi(api_client.Api):
 
     def _post_operators_oapg(
