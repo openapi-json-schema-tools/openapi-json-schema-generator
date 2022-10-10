688c7aa12015b283afc75914aa729ff45d854aab
Justin Black <justin.a.black@gmail.com>
2022-10-10 11:35:33 -0700

    Samples updated

--- samples/openapi3/client/features/nonCompliantUseDiscriminatorIfCompositionFails/python/setup.py
+++ samples/openapi3/client/features/nonCompliantUseDiscriminatorIfCompositionFails/python/setup.py
@@ -21,12 +21,12 @@ VERSION = "1.0.0"
 # http://pypi.python.org/pypi/setuptools
 
 REQUIRES = [
-    "certifi ~= 14.5.14",
+    "certifi >= 14.5.14",
     "frozendict ~= 2.3.4",
     "python-dateutil ~= 2.7.0",
-    "setuptools ~= 21.0.0",
+    "setuptools >= 21.0.0",
     "typing_extensions ~= 4.3.0",
-    "urllib3 ~= 1.26.12",
+    "urllib3 ~= 1.26.7",
 ]
 
 setup(
