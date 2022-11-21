b98f5e6d0285887be0b22fad21eb538c30e4dee9
Justin Black <spacether@users.noreply.github.com>
2022-11-21 11:21:09 -0500

    v2 adds generics, OpenApiResponse + ApiException updates (#86)
    * Adds generics for mypy
    
    * Adds generic to exception
    
    * Removes api_response from ApiException
    
    * Fixes tests
    
    * Samples regenerated
    
    * Fixes exceptions.ApiException reason, body, and headers

--- samples/openapi3/client/features/nonCompliantUseDiscriminatorIfCompositionFails/python/this_package/paths/operators/post/__init__.py
+++ samples/openapi3/client/features/nonCompliantUseDiscriminatorIfCompositionFails/python/this_package/paths/operators/post/__init__.py
@@ -32,9 +32,16 @@ from . import response_for_200
 from . import request_body
 
 
-_status_code_to_response = {
+
+__StatusCodeToResponse = typing_extensions.TypedDict(
+    '__StatusCodeToResponse',
+    {
+        '200': api_client.OpenApiResponse[response_for_200.ApiResponse],
+    }
+)
+_status_code_to_response = __StatusCodeToResponse({
     '200': response_for_200.response,
-}
+})
 
 
 class BaseApi(api_client.Api):
@@ -126,14 +133,21 @@ class BaseApi(api_client.Api):
         if skip_deserialization:
             api_response = api_client.ApiResponseWithoutDeserialization(response=response)
         else:
-            response_for_status = _status_code_to_response.get(str(response.status))
-            if response_for_status:
-                api_response = response_for_status.deserialize(response, self.api_client.configuration)
+            status = str(response.status)
+            if status in _status_code_to_response:
+                status: typing_extensions.Literal[
+                    '200',
+                ]
+                api_response = _status_code_to_response[status].deserialize(response, self.api_client.configuration)
             else:
                 api_response = api_client.ApiResponseWithoutDeserialization(response=response)
 
         if not 200 <= response.status <= 299:
-            raise exceptions.ApiException(api_response=api_response)
+            raise exceptions.ApiException(
+                status=response.status,
+                reason=response.reason,
+                api_response=api_response
+            )
 
         return api_response
 
--- samples/openapi3/client/petstore/python/petstore_api/paths/user_username/put/__init__.py
+++ samples/openapi3/client/petstore/python/petstore_api/paths/user_username/put/__init__.py
@@ -57,10 +57,18 @@ class RequestPathParameters:
     parameters = [
         parameter_0.parameter_oapg,
     ]
-_status_code_to_response = {
+
+__StatusCodeToResponse = typing_extensions.TypedDict(
+    '__StatusCodeToResponse',
+    {
+        '400': api_client.OpenApiResponse[response_for_400.ApiResponse],
+        '404': api_client.OpenApiResponse[response_for_404.ApiResponse],
+    }
+)
+_status_code_to_response = __StatusCodeToResponse({
     '400': response_for_400.response,
     '404': response_for_404.response,
-}
+})
 
 
 class BaseApi(api_client.Api):
@@ -165,14 +173,22 @@ class BaseApi(api_client.Api):
         if skip_deserialization:
             api_response = api_client.ApiResponseWithoutDeserialization(response=response)
         else:
-            response_for_status = _status_code_to_response.get(str(response.status))
-            if response_for_status:
-                api_response = response_for_status.deserialize(response, self.api_client.configuration)
+            status = str(response.status)
+            if status in _status_code_to_response:
+                status: typing_extensions.Literal[
+                    '400',
+                    '404',
+                ]
+                api_response = _status_code_to_response[status].deserialize(response, self.api_client.configuration)
             else:
                 api_response = api_client.ApiResponseWithoutDeserialization(response=response)
 
         if not 200 <= response.status <= 299:
-            raise exceptions.ApiException(api_response=api_response)
+            raise exceptions.ApiException(
+                status=response.status,
+                reason=response.reason,
+                api_response=api_response
+            )
 
         return api_response
 
