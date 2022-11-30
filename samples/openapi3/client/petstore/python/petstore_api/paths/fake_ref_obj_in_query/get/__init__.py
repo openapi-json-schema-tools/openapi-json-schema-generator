234b53d3ae191a98e9794c4ab7bcc05316a74b92
Justin Black <justin.a.black@gmail.com>
2022-11-29 17:00:50 -0800

    Uses refed responses if they exist

--- samples/openapi3/client/petstore/python/petstore_api/paths/fake_ref_obj_in_query/get/__init__.py
+++ samples/openapi3/client/petstore/python/petstore_api/paths/fake_ref_obj_in_query/get/__init__.py
@@ -23,9 +23,9 @@ import uuid  # noqa: F401
 import frozendict  # noqa: F401
 
 from petstore_api import schemas  # noqa: F401
+from petstore_api.components.responses import success_description_only_response as response_for_200
 
 from .. import path
-from . import response_for_200
 from . import parameter_0
 
 
