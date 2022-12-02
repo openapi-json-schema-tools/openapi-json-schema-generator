befc3bfaf7fa4d698971dfa20bfe46ffba7a943a
Justin Black <justin.a.black@gmail.com>
2022-12-02 13:19:33 -0800

    Fixes test_dict_validate

--- samples/openapi3/client/petstore/python/tests_manual/test_validate.py
+++ samples/openapi3/client/petstore/python/tests_manual/test_validate.py
@@ -90,7 +90,6 @@ class TestValidateResults(unittest.TestCase):
         assert path_to_schemas == {
             ("args[0]",): {Foo, frozendict.frozendict},
             ("args[0]", "bar"): {StrSchema, str},
-            ("args[0]", "additional"): {schemas.UnsetAnyTypeSchema, decimal.Decimal},
         }
 
     def test_discriminated_dict_validate(self):
