01e34ba7b9ed03e7d1fa29b8f41d165455a4f12a
Justin Black <justin.a.black@gmail.com>
2022-12-24 14:39:49 -0800

    Renegerates schema tests

--- samples/openapi3/client/petstore/python/test/components/schema/test_model_return.py
+++ samples/openapi3/client/petstore/python/test/components/schema/test_model_return.py
@@ -12,7 +12,7 @@
 import unittest
 
 import petstore_api
-from petstore_api.components.schema.model_return import ModelReturn
+from petstore_api.components.schema._return import ModelReturn
 from petstore_api import configuration
 
 
--- samples/openapi3/client/petstore/python/test/components/schema/test_special_model_name.py
+++ samples/openapi3/client/petstore/python/test/components/schema/test_special_model_name.py
@@ -12,7 +12,7 @@
 import unittest
 
 import petstore_api
-from petstore_api.components.schema.special_model_name import SpecialModelName
+from petstore_api.components.schema.special_model_name_ import SpecialModelName
 from petstore_api import configuration
 
 
