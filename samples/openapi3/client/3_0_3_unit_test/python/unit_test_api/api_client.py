60a77c9e7f0bcee01bce194066eb09cd05b00f75
Justin Black <justin.a.black@gmail.com>
2022-12-30 17:34:54 -0800

    Samples regenerated

--- samples/openapi3/client/3_0_3_unit_test/python/unit_test_api/api_client.py
+++ samples/openapi3/client/3_0_3_unit_test/python/unit_test_api/api_client.py
@@ -982,7 +982,7 @@ class ApiClient:
             self.default_headers[header_name] = header_value
         self.cookie = cookie
         # Set default User-Agent.
-        self.user_agent = 'OpenAPI-Generator/1.0.0/python'
+        self.user_agent = 'OpenAPI-JSON-Schema-Generator/1.0.0/python'
 
     def __enter__(self):
         return self
