dc4e8acb7b3505a6e989854817544a49d230542d
Justin Black <justin.a.black@gmail.com>
2023-03-03 13:36:32 -0800

    Fixes java test

--- modules/openapi-json-schema-generator/src/test/java/org/openapijsonschematools/codegen/DefaultCodegenTest.java
+++ modules/openapi-json-schema-generator/src/test/java/org/openapijsonschematools/codegen/DefaultCodegenTest.java
@@ -2102,11 +2102,15 @@ public class DefaultCodegenTest {
         codegen.setOpenAPI(openAPI);
 
         final Map<String, SecurityScheme> securitySchemes = openAPI.getComponents().getSecuritySchemes();
-        final List<CodegenSecurityScheme> securities = codegen.fromSecurityScheme(securitySchemes);
-
-        assertEquals(securities.size(), 2);
-        final List<String> flows = securities.stream().map(c -> c.flow).collect(Collectors.toList());
-        assertTrue(flows.containsAll(Arrays.asList("password", "application")));
+        HashMap<String, CodegenSecurityScheme> securities = new HashMap<>();
+        for (Map.Entry<String, SecurityScheme> entry: securitySchemes.entrySet()) {
+            String componentName = entry.getKey();
+            CodegenSecurityScheme cs = codegen.fromSecurityScheme(entry.getValue(), "#/components/securitySchemes/" + componentName);
+            securities.put(componentName, cs);
+            assertNotNull(cs.flows.password);
+            assertNotNull(cs.flows.clientCredentials);
+        }
+        assertEquals(securities.size(), 1);
     }
 
     @Test
