0380318a5c4b521162a3151e16cb190097309f60
Justin Black <justin.a.black@gmail.com>
2023-04-26 16:38:10 -0700

    Samples updated

--- samples/openapi3/client/features/security/python/src/this_package/paths/path_with_no_explicit_security/get/operation.py
+++ samples/openapi3/client/features/security/python/src/this_package/paths/path_with_no_explicit_security/get/operation.py
@@ -123,97 +123,9 @@ class BaseApi(api_client.Api):
 
 class PathWithNoExplicitSecurity(BaseApi):
     # this class is used by api classes that refer to endpoints with operationId.snakeCase fn names
-
-    @typing.overload
-    def path_with_no_explicit_security(
-        self,
-        server_index: typing.Optional[int] = None,
-        stream: bool = False,
-        timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
-        skip_deserialization: typing_extensions.Literal[False] = ...,
-    ) -> response_200.ResponseFor200.response_cls: ...
-
-    @typing.overload
-    def path_with_no_explicit_security(
-        self,
-        skip_deserialization: typing_extensions.Literal[True],
-        server_index: typing.Optional[int] = None,
-        stream: bool = False,
-        timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
-    ) -> api_client.ApiResponseWithoutDeserialization: ...
-
-    @typing.overload
-    def path_with_no_explicit_security(
-        self,
-        server_index: typing.Optional[int] = None,
-        stream: bool = False,
-        timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
-        skip_deserialization: bool = ...,
-    ) -> typing.Union[
-        response_200.ResponseFor200.response_cls,
-        api_client.ApiResponseWithoutDeserialization,
-    ]: ...
-
-    def path_with_no_explicit_security(
-        self,
-        server_index: typing.Optional[int] = None,
-        stream: bool = False,
-        timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
-        skip_deserialization: bool = False,
-    ):
-        return self._path_with_no_explicit_security(
-            server_index=server_index,
-            stream=stream,
-            timeout=timeout,
-            skip_deserialization=skip_deserialization
-        )
+    path_with_no_explicit_security = BaseApi._path_with_no_explicit_security
 
 
 class ApiForGet(BaseApi):
     # this class is used by api classes that refer to endpoints by path and http method names
-
-    @typing.overload
-    def get(
-        self,
-        server_index: typing.Optional[int] = None,
-        stream: bool = False,
-        timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
-        skip_deserialization: typing_extensions.Literal[False] = ...,
-    ) -> response_200.ResponseFor200.response_cls: ...
-
-    @typing.overload
-    def get(
-        self,
-        skip_deserialization: typing_extensions.Literal[True],
-        server_index: typing.Optional[int] = None,
-        stream: bool = False,
-        timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
-    ) -> api_client.ApiResponseWithoutDeserialization: ...
-
-    @typing.overload
-    def get(
-        self,
-        server_index: typing.Optional[int] = None,
-        stream: bool = False,
-        timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
-        skip_deserialization: bool = ...,
-    ) -> typing.Union[
-        response_200.ResponseFor200.response_cls,
-        api_client.ApiResponseWithoutDeserialization,
-    ]: ...
-
-    def get(
-        self,
-        server_index: typing.Optional[int] = None,
-        stream: bool = False,
-        timeout: typing.Optional[typing.Union[int, typing.Tuple]] = None,
-        skip_deserialization: bool = False,
-    ):
-        return self._path_with_no_explicit_security(
-            server_index=server_index,
-            stream=stream,
-            timeout=timeout,
-            skip_deserialization=skip_deserialization
-        )
-
-
+    get = BaseApi._path_with_no_explicit_security
