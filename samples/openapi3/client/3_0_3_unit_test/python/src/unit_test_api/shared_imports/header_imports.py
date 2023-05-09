923d1380e5c0641c583b84a97236e5672ed1b3f9
Justin Black <spacether@users.noreply.github.com>
2023-05-09 16:39:17 -0700

    v3 code reduction w/ shared_imports (#168)
    * Adds typing_imports
    
    * Adds typing_imports for header and parameter
    
    * Updates request body imports
    
    * Adds response_imports
    
    * Adds schema_imports
    
    * Adds security_scheme_imports
    
    * Adds server imports
    
    * Adds operation_imports
    
    * typing_imports -> header_imports
    
    * Sample regen

---
+++ samples/openapi3/client/3_0_3_unit_test/python/src/unit_test_api/shared_imports/header_imports.py
@@ -0,0 +1,4 @@
+import typing
+import typing_extensions
+
+from unit_test_api import api_client
---
+++ samples/openapi3/client/3_0_3_unit_test/python/src/unit_test_api/shared_imports/schema_imports.py
@@ -0,0 +1,11 @@
+import datetime
+import decimal
+import io
+import re
+import typing
+import typing_extensions
+import uuid
+
+import frozendict
+
+from unit_test_api import schemas
