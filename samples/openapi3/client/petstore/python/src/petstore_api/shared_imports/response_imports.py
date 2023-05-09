5ca8ce02435f71c9926cd7045ceef26b8b5c6f4e
Justin Black <spacether@users.noreply.github.com>
2023-05-09 16:39:17 -0700

    v3 code reduction w/ shared_imports (#168)
    * Adds typing_imports
    
    * Adds typing_imports for header and parameter
    
    * Updates request body imports
    
    * Adds response_imports
    
    * Adds schema_imports
    
    * Adds security_scheme_imports
    
    * Adds server imports
    
    * Adds operation_imports
    
    * typing_imports -> header_imports
    
    * Sample regen

---
+++ samples/openapi3/client/petstore/python/src/petstore_api/shared_imports/response_imports.py
@@ -0,0 +1,12 @@
+import dataclasses
+import datetime
+import decimal
+import io
+import typing
+import uuid
+
+import frozendict
+import typing_extensions
+import urllib3
+
+from petstore_api import api_client, schemas
