87f20b0fa2800a56d226794b504931f61a3e390e
Justin Black <justin.a.black@gmail.com>
2023-05-22 16:53:21 -0700

    Samples regenerated

--- samples/openapi3/client/3_0_3_unit_test/python/test/test_paths/test_request_body_post_nested_oneof_to_check_validation_semantics_request_body/test_post.py
+++ samples/openapi3/client/3_0_3_unit_test/python/test/test_paths/test_request_body_post_nested_oneof_to_check_validation_semantics_request_body/test_post.py
@@ -8,6 +8,7 @@ import unittest
 from unittest.mock import patch
 
 import urllib3
+import typing_extensions
 
 import unit_test_api
 from unit_test_api.paths.request_body_post_nested_oneof_to_check_validation_semantics_request_body.post import operation as post  # noqa: E501
