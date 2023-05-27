06ff0e51e15136beff8ac14f03a852f9d1f8c855
Justin Black <justin.a.black@gmail.com>
2023-05-27 14:30:56 -0700

    Adds parameterization to reuired ref properties in schema new

--- samples/openapi3/client/petstore/python/src/petstore_api/components/schema/money.py
+++ samples/openapi3/client/petstore/python/src/petstore_api/components/schema/money.py
@@ -84,8 +84,9 @@ class Money(
             str
         ],
         currency: typing.Union[
-            currency.Currency,
-            str        ],
+            currency.Currency[str],
+            str
+        ],
         configuration_: typing.Optional[schemas.schema_configuration.SchemaConfiguration] = None,
         **kwargs: typing.Union[dict, frozendict.frozendict, list, tuple, decimal.Decimal, float, int, str, datetime.date, datetime.datetime, uuid.UUID, bool, None, bytes, io.FileIO, io.BufferedReader, schemas.Schema],
     ) -> Money[frozendict.frozendict]:
