d83d3e5a4dacd42aab26736d017dd66ad7c65a12
Justin Black <justin.a.black@gmail.com>
2023-07-20 11:14:39 -0700

    Removes example module

--- modules/openapi-json-schema-generator/src/test/java/org/openapijsonschematools/codegen/utils/SerializerUtilsTest.java
+++ modules/openapi-json-schema-generator/src/test/java/org/openapijsonschematools/codegen/utils/SerializerUtilsTest.java
@@ -1,4 +1,4 @@
-package org.openapijsonschematools.codegen.serializer;
+package org.openapijsonschematools.codegen.utils;
 
 import io.swagger.v3.oas.models.*;
 import io.swagger.v3.oas.models.info.Info;
