6da30219b2c751cfb65ab56bd3be3b219f3ddc62
Justin Black <justin.a.black@gmail.com>
2023-07-24 17:04:50 -0700

    Fixes more java tests, removes more unused samples

--- src/test/java/org/openapijsonschematools/codegen/generators/DefaultGeneratorTest.java
+++ src/test/java/org/openapijsonschematools/codegen/generators/DefaultGeneratorTest.java
@@ -280,7 +280,7 @@ public class DefaultGeneratorTest {
     @Test
     public void testOriginalOpenApiDocumentVersion() {
         // Test with OAS 2.0 document.
-        String location = "src/test/resources/2_0/python-prior/petstore-with-fake-endpoints-models-for-testing.yaml";
+        String location = "src/test/resources/2_0/sample_spec.yml";
         OpenAPI openAPI = TestUtils.parseFlattenSpec(location);
         SemVer version = ModelUtils.getOpenApiVersion(openAPI, location, null);
         Assert.assertEquals(version, new SemVer("2.0.0"));
@@ -1295,7 +1295,7 @@ public class DefaultGeneratorTest {
 
     @Test
     public void verifyXDiscriminatorValue() {
-        final OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/2_0/x-discriminator-value.yaml");
+        final OpenAPI openAPI = TestUtils.parseFlattenSpec("src/test/resources/3_0/x-discriminator-value.yaml");
         final DefaultGenerator config = new DefaultGenerator();
         config.setOpenAPI(openAPI);
 
