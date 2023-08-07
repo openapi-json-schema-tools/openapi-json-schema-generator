b0438de79303cddcd806dd18c41674dac10fc5a0
Justin Black <spacether@users.noreply.github.com>
2023-08-07 12:59:07 -0700

    CircleCi cleanup (#198)
    * Changes nodeNo to nodeId
    
    * Replaces node3 with node4
    
    * Updates circleci cleanup fn
    
    * Changes nodeId to jobId
    
    * FIxes parallel.sh
    
    * Renames job3
    
    * Fixes lingering ref to node3
    
    * Updates job1 name
    
    * Removes other unused job
    
    * Removes unnecesary docker petstore server in circleci

--- .circleci/parallel.sh
+++ .circleci/parallel.sh
@@ -3,77 +3,29 @@
 # A bash script to run CircleCI node/test in parallel
 #
 
-NODE_INDEX=${CIRCLE_NODE_INDEX:-0}
+JOB_ID=${CIRCLE_JOB_ID:-"unset"}
 
 set -e
 
 export NODE_ENV=test
 
-function cleanup {
-  # Show logs of 'petstore.swagger' container to troubleshoot Unit Test failures, if any.
-  if [ "$NODE_INDEX" != "4" ]; then
-    docker logs petstore.swagger # container name specified in circle.yml
-  fi
-}
-
-trap cleanup EXIT
-
-if [ "$NODE_INDEX" = "1" ]; then
-  # node1
-  # ensures samples + docs up to date
-  echo "Running node $NODE_INDEX"
-
+if [ "$JOB_ID" = "ensureSamplesAndGeneratorDocsUpToDate" ]; then
+  echo "Running job $JOB_ID"
   ./bin/utils/ensure-up-to-date
 
-elif [ "$NODE_INDEX" = "2" ]; then
-  # node2
-  echo "Running node $NODE_INDEX"
+elif [ "$JOB_ID" = "mvnCleanInstall" ]; then
+  echo "Running job $JOB_ID"
+  mvn --no-snapshot-updates --quiet clean install -Dorg.slf4j.simpleLogger.defaultLogLevel=error
 
-elif [ "$NODE_INDEX" = "3" ]; then
-  # node3
-
-  echo "Running node $NODE_INDEX to test 'samples.circleci.node3' defined in pom.xml ..."
-  #wget https://www.python.org/ftp/python/3.8.9/Python-3.8.9.tgz
-  #tar -xf Python-3.8.9.tgz
-  #cd Python-3.8.9
-  #./configure --enable-optimizations
-  #sudo make altinstall
-  pyenv install --list 
-  pyenv install 3.6.3
-  pyenv install 2.7.14
-  pyenv global 3.6.3
-
-  # Install node@stable (for angular 6)
-  set +e
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-  export NVM_DIR="/opt/circleci/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  #nvm install stable
-  # install v16 instead of the latest stable version
-  nvm install 16
-  nvm alias default 16
-  node --version
-
-  # Each step uses the same `$BASH_ENV`, so need to modify it
-  echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
-  echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV
-
-  # no longer testing python-prior
-  #mvn --no-snapshot-updates --quiet verify -Psamples.circleci.node3 -Dorg.slf4j.simpleLogger.defaultLogLevel=error
-
-elif [ "$NODE_INDEX" = "4" ]; then
-  # node4
-
-  echo "Running node $NODE_INDEX to test 'samples.circleci.node4' defined in pom.xml ..."
-
-  #mvn --no-snapshot-updates --quiet verify -Psamples.circleci.node4 -Dorg.slf4j.simpleLogger.defaultLogLevel=error
+elif [ "$JOB_ID" = "testPythonClientSamples" ]; then
+  echo "Running job $JOB_ID ..."
   (cd samples/client/petstore/python && make test)
   (cd samples/client/3_0_3_unit_test/python && make test)
   (cd samples/client/openapi_features/nonCompliantUseDiscriminatorIfCompositionFails/python && make test)
   (cd samples/client/openapi_features/security/python && make test)
 
 else
-  echo "Running node $NODE_INDEX"
+  echo "Running job $JOB_ID"
 
 fi
 
