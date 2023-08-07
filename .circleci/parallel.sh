d5dc586c96a61b54992ce9dde34a7834c2596867
Justin Black <justin.a.black@gmail.com>
2023-08-07 12:15:04 -0700

    Changes nodeId to jobId

--- .circleci/parallel.sh
+++ .circleci/parallel.sh
@@ -3,7 +3,7 @@
 # A bash script to run CircleCI node/test in parallel
 #
 
-NODE_ID=${CIRCLE_NODE_ID:-0}
+JOB_ID=${CIRCLE_JOB_ID:-0}
 
 set -e
 
@@ -11,28 +11,28 @@ export NODE_ENV=test
 
 function cleanup {
   # Show logs of 'petstore.swagger' container to troubleshoot Unit Test failures, if any.
-  if [ "$NODE_ID" != "3" ]; then
+  if [ "$JOB_ID" != "3" ]; then
     docker logs petstore.swagger # container name specified in circle.yml
   fi
 }
 
 trap cleanup EXIT
 
-if [ "$NODE_ID" = "1" ]; then
+if [ "$JOB_ID" = "1" ]; then
   # node1
   # ensures samples + docs up to date
-  echo "Running node $NODE_ID"
+  echo "Running job $JOB_ID"
 
   ./bin/utils/ensure-up-to-date
 
 elif [ "$NODE_ID" = "2" ]; then
   # node2
-  echo "Running node $NODE_ID"
+  echo "Running job $JOB_ID"
 
 elif [ "$NODE_ID" = "3" ]; then
   # node4
 
-  echo "Running node $NODE_ID to test python client samples ..."
+  echo "Running job $JOB_ID to test python client samples ..."
 
   (cd samples/client/petstore/python && make test)
   (cd samples/client/3_0_3_unit_test/python && make test)
@@ -40,7 +40,7 @@ elif [ "$NODE_ID" = "3" ]; then
   (cd samples/client/openapi_features/security/python && make test)
 
 else
-  echo "Running node $NODE_ID"
+  echo "Running job $JOB_ID"
 
 fi
 
