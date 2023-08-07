d53174d0292a71a818ff3d4bdb1ad2dd7bd3037e
Justin Black <justin.a.black@gmail.com>
2023-08-07 12:01:36 -0700

    Replaces node3 with node4

--- .circleci/parallel.sh
+++ .circleci/parallel.sh
@@ -30,43 +30,10 @@ elif [ "$NODE_ID" = "2" ]; then
   echo "Running node $NODE_ID"
 
 elif [ "$NODE_ID" = "3" ]; then
-  # node3
-
-  echo "Running node $NODE_ID to test 'samples.circleci.node3' defined in pom.xml ..."
-  #wget https://www.python.org/ftp/python/3.8.9/Python-3.8.9.tgz
-  #tar -xf Python-3.8.9.tgz
-  #cd Python-3.8.9
-  #./configure --enable-optimizations
-  #sudo make altinstall
-  pyenv install --list 
-  pyenv install 3.6.3
-  pyenv install 2.7.14
-  pyenv global 3.6.3
-
-  # Install node@stable (for angular 6)
-  set +e
-  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-  export NVM_DIR="/opt/circleci/.nvm"
-  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
-  #nvm install stable
-  # install v16 instead of the latest stable version
-  nvm install 16
-  nvm alias default 16
-  node --version
-
-  # Each step uses the same `$BASH_ENV`, so need to modify it
-  echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
-  echo "[ -s \"$NVM_DIR/nvm.sh\" ] && . \"$NVM_DIR/nvm.sh\"" >> $BASH_ENV
-
-  # no longer testing python-prior
-  #mvn --no-snapshot-updates --quiet verify -Psamples.circleci.node3 -Dorg.slf4j.simpleLogger.defaultLogLevel=error
-
-elif [ "$NODE_ID" = "4" ]; then
   # node4
 
-  echo "Running node $NODE_ID to test 'samples.circleci.node4' defined in pom.xml ..."
+  echo "Running node $NODE_ID to test python client samples ..."
 
-  #mvn --no-snapshot-updates --quiet verify -Psamples.circleci.node4 -Dorg.slf4j.simpleLogger.defaultLogLevel=error
   (cd samples/client/petstore/python && make test)
   (cd samples/client/3_0_3_unit_test/python && make test)
   (cd samples/client/openapi_features/nonCompliantUseDiscriminatorIfCompositionFails/python && make test)
