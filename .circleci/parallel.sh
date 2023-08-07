cfb0d8796b7fb0288bb2b6ec92a5dd4e24463681
Justin Black <justin.a.black@gmail.com>
2023-08-07 12:46:01 -0700

    Removes other unused job

--- .circleci/parallel.sh
+++ .circleci/parallel.sh
@@ -3,7 +3,7 @@
 # A bash script to run CircleCI node/test in parallel
 #
 
-JOB_ID=${CIRCLE_JOB_ID:-0}
+JOB_ID=${CIRCLE_JOB_ID:-"unset"}
 
 set -e
 
@@ -19,20 +19,15 @@ function cleanup {
 trap cleanup EXIT
 
 if [ "$JOB_ID" = "ensureSamplesAndGeneratorDocsUpToDate" ]; then
-  # node1
-  # ensures samples + docs up to date
   echo "Running job $JOB_ID"
-
   ./bin/utils/ensure-up-to-date
 
-elif [ "$JOB_ID" = "2" ]; then
-  # node2
+elif [ "$JOB_ID" = "mvnCleanInstall" ]; then
   echo "Running job $JOB_ID"
+  mvn --no-snapshot-updates --quiet clean install -Dorg.slf4j.simpleLogger.defaultLogLevel=error
 
 elif [ "$JOB_ID" = "testPythonClientSamples" ]; then
-
   echo "Running job $JOB_ID ..."
-
   (cd samples/client/petstore/python && make test)
   (cd samples/client/3_0_3_unit_test/python && make test)
   (cd samples/client/openapi_features/nonCompliantUseDiscriminatorIfCompositionFails/python && make test)
