963ad5a5fa628962a6981f6fd292c3b572177379
Justin Black <justin.a.black@gmail.com>
2023-08-21 16:15:37 -0700

    Fixes validate else conditions

--- samples/client/3_1_0_json_schema/python/src/json_schema_api/components/schema/array_min_contains_value.py
+++ samples/client/3_1_0_json_schema/python/src/json_schema_api/components/schema/array_min_contains_value.py
@@ -34,6 +34,20 @@ class Contains(
     )
     enums = ContainsEnums
 
+    @typing.overload
+    @classmethod
+    def validate(
+        cls,
+        arg: typing.Literal[1],
+        configuration: typing.Optional[schema_configuration.SchemaConfiguration] = None
+    ) -> typing.Literal[1]: ...
+    @typing.overload
+    @classmethod
+    def validate(
+        cls,
+        arg: int,
+        configuration: typing.Optional[schema_configuration.SchemaConfiguration] = None
+    ) -> typing.Literal[1,]: ...
     @classmethod
     def validate(
         cls,
