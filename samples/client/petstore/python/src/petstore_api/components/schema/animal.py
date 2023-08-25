293254d69da8c943defa21f27c41af7663fb501b
Justin Black <justin.a.black@gmail.com>
2023-08-25 14:46:02 -0700

    Switches dict schema info order back to original

--- samples/client/petstore/python/src/petstore_api/components/schema/animal.py
+++ samples/client/petstore/python/src/petstore_api/components/schema/animal.py
@@ -109,7 +109,6 @@ class Animal(
     required: typing.FrozenSet[str] = frozenset({
         "className",
     })
-    properties: Properties = dataclasses.field(default_factory=lambda: schemas.typed_dict_to_instance(Properties)) # type: ignore
     discriminator: typing.Mapping[str, typing.Mapping[str, typing.Type[schemas.Schema]]] = dataclasses.field(
         default_factory=lambda: {
             'className': {
@@ -118,6 +117,7 @@ class Animal(
             }
         }
     )
+    properties: Properties = dataclasses.field(default_factory=lambda: schemas.typed_dict_to_instance(Properties)) # type: ignore
     type_to_output_cls: typing.Mapping[
         typing.Type,
         typing.Type
