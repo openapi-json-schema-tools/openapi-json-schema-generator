cbf546bf1ade270da0ca0947044f57dc2d0e1158
Justin Black <justin.a.black@gmail.com>
2023-11-15 13:47:23 -0800

    Adds needed imports to ref schema files

--- samples/client/petstore/java/src/main/java/org/openapijsonschematools/paths/userusername/get/responses/response200/content/applicationxml/Schema.java
+++ samples/client/petstore/java/src/main/java/org/openapijsonschematools/paths/userusername/get/responses/response200/content/applicationxml/Schema.java
@@ -1,5 +1,15 @@
 package org.openapijsonschematools.paths.userusername.get.responses.response200.content.applicationxml;
+import org.openapijsonschematools.configurations.SchemaConfiguration;
+import org.openapijsonschematools.schemas.JsonSchema;
+import org.openapijsonschematools.schemas.JsonSchemas;
+import org.openapijsonschematools.schemas.FrozenList;
+import org.openapijsonschematools.schemas.FrozenMap;
 
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
 public class Schema {
     // $refed class
     
