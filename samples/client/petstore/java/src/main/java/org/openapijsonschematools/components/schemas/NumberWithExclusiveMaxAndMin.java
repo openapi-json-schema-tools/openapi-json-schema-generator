d76bb6f45b6f97279bb1880057297dbd4e808187
Justin Black <justin.a.black@gmail.com>
2023-11-28 14:07:20 -0800

    Removes number with exc max and min constraints

--- samples/client/petstore/java/src/main/java/org/openapijsonschematools/components/schemas/NumberWithExclusiveMaxAndMin.java
+++
@@ -1,9 +0,0 @@
-package org.openapijsonschematools.components.schemas;
-import org.openapijsonschematools.schemas.NumberJsonSchema;
-
-public class NumberWithExclusiveMaxAndMin {
-    // nest classes so all schemas and input/output classes can be public
-    
-    
-    public static class NumberWithExclusiveMaxAndMin1 extends NumberJsonSchema {}
-}
