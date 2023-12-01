90c525980c3032d4ad8664b8af05c1a7a66291d1
Justin Black <justin.a.black@gmail.com>
2023-12-01 12:28:41 -0800

    Samples regen

--- samples/client/petstore/java/src/main/java/org/openapijsonschematools/schemas/validation/EnumValidator.java
+++ samples/client/petstore/java/src/main/java/org/openapijsonschematools/schemas/validation/EnumValidator.java
@@ -22,23 +22,22 @@ public class EnumValidator implements KeywordValidator {
         if (enumValues.isEmpty()) {
             throw new ValidationException("No value can match enum because enum is empty");
         }
-        if (!enumValues.contains(arg)) {
-            ValidationException exc = new ValidationException("Invalid value "+arg+" was not one of the allowed enum "+enumValues);
-            if (arg instanceof Float) {
-                BigDecimal castArg = BigDecimal.valueOf((Float) arg);
-                boolean hasIntValue = castArg.stripTrailingZeros().scale() <= 0;
-                if (hasIntValue && !enumValues.contains(castArg.intValue())) {
-                    throw exc;
-                }
-            } else if (arg instanceof Double) {
-                BigDecimal castArg = new BigDecimal(String.valueOf(arg));
-                boolean hasIntValue = castArg.stripTrailingZeros().scale() <= 0;
-                if (hasIntValue && !enumValues.contains(castArg.intValue())) {
-                    throw exc;
-                }
-            } else {
-                throw exc;
+        if (enumValues.contains(arg)) {
+            return null;
+        }
+        if (arg instanceof Float) {
+            BigDecimal castArg = BigDecimal.valueOf((Float) arg);
+            boolean hasIntValue = castArg.stripTrailingZeros().scale() <= 0;
+            if (hasIntValue && enumValues.contains(castArg.intValue())) {
+                return null;
+            }
+        } else if (arg instanceof Double) {
+            BigDecimal castArg = new BigDecimal(String.valueOf(arg));
+            boolean hasIntValue = castArg.stripTrailingZeros().scale() <= 0;
+            if (hasIntValue && enumValues.contains(castArg.intValue())) {
+                return null;
             }
         }
-        return null;
-    }}
+        throw new ValidationException("Invalid value "+arg+" was not one of the allowed enum "+enumValues);
+    }
+}
