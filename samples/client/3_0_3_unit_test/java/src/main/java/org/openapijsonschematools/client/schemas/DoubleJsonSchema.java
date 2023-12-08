54e3fdca200eb6fe0b68d77a25811875a42206de
Justin Black <justin.a.black@gmail.com>
2023-12-07 18:45:37 -0800

    Updates decimal and double json schemas

--- samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/DoubleJsonSchema.java
+++ samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/DoubleJsonSchema.java
@@ -3,7 +3,6 @@ package org.openapijsonschematools.client.schemas;
 import org.openapijsonschematools.client.schemas.validation.JsonSchema;
 import org.openapijsonschematools.client.configurations.SchemaConfiguration;
 import org.openapijsonschematools.client.schemas.validation.KeywordEntry;
-import org.openapijsonschematools.client.schemas.validation.KeywordValidator;
 import org.openapijsonschematools.client.schemas.validation.TypeValidator;
 import org.openapijsonschematools.client.schemas.validation.FormatValidator;
 import org.openapijsonschematools.client.exceptions.ValidationException;
@@ -12,14 +11,15 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
-
 public class DoubleJsonSchema extends JsonSchema {
-    public static final LinkedHashMap<String, KeywordValidator> keywordToValidator = new LinkedHashMap<>(Map.ofEntries(
-        new KeywordEntry("type", new TypeValidator(Set.of(Double.class))),
-        new KeywordEntry("format", new FormatValidator("double"))
-    ));
+    public DoubleJsonSchema() {
+        keywordToValidator = new LinkedHashMap<>(Map.ofEntries(
+                new KeywordEntry("type", new TypeValidator(Set.of(Double.class))),
+                new KeywordEntry("format", new FormatValidator("double"))
+        ));
+    }
 
-    public static double validate(double arg, SchemaConfiguration configuration) throws ValidationException {
-        return JsonSchema.validateDouble(DoubleJsonSchema.class, arg, configuration);
+    public double validate(double arg, SchemaConfiguration configuration) throws ValidationException {
+        return validateDouble(arg, configuration);
     }
 }
