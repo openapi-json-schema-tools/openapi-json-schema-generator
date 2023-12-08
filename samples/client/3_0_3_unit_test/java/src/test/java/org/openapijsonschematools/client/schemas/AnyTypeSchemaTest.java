3b9a80d77db6ab5f41a5de4dbdb1204ae23c3fcc
Justin Black <justin.a.black@gmail.com>
2023-12-07 20:43:25 -0800

    Fixes anytypeschematest

--- samples/client/3_0_3_unit_test/java/src/test/java/org/openapijsonschematools/client/schemas/AnyTypeSchemaTest.java
+++ samples/client/3_0_3_unit_test/java/src/test/java/org/openapijsonschematools/client/schemas/AnyTypeSchemaTest.java
@@ -16,62 +16,63 @@ import java.util.LinkedHashMap;
 
 
 public class AnyTypeSchemaTest {
+    static final AnyTypeJsonSchema schema = JsonSchemaFactory.getInstance(AnyTypeJsonSchema.class);
     static final SchemaConfiguration configuration = new SchemaConfiguration(JsonSchemaKeywordFlags.ofNone());
 
     @Test
     public void testValidateNull() {
-        Void validatedValue = AnyTypeJsonSchema.validate((Void) null, configuration);
+        Void validatedValue = schema.validate((Void) null, configuration);
         Assert.assertNull(validatedValue);
     }
 
     @Test
     public void testValidateBoolean() {
-        boolean trueValue = AnyTypeJsonSchema.validate(true, configuration);
-        Assert.assertEquals(trueValue, true);
+        boolean trueValue = schema.validate(true, configuration);
+        Assert.assertTrue(trueValue);
 
-        boolean falseValue = AnyTypeJsonSchema.validate(false, configuration);
-        Assert.assertEquals(falseValue, false);
+        boolean falseValue = schema.validate(false, configuration);
+        Assert.assertFalse(falseValue);
     }
 
     @Test
     public void testValidateInteger() {
-        int validatedValue = AnyTypeJsonSchema.validate(1, configuration);
+        int validatedValue = schema.validate(1, configuration);
         Assert.assertEquals(validatedValue, 1);
     }
 
     @Test
     public void testValidateLong() {
-        long validatedValue = AnyTypeJsonSchema.validate(1L, configuration);
+        long validatedValue = schema.validate(1L, configuration);
         Assert.assertEquals(validatedValue, 1L);
     }
 
     @Test
     public void testValidateFloat() {
-        float validatedValue = AnyTypeJsonSchema.validate(3.14f, configuration);
+        float validatedValue = schema.validate(3.14f, configuration);
         Assert.assertEquals(Float.compare(validatedValue, 3.14f), 0);
     }
 
     @Test
     public void testValidateDouble() {
-        double validatedValue = AnyTypeJsonSchema.validate(70.6458763d, configuration);
+        double validatedValue = schema.validate(70.6458763d, configuration);
         Assert.assertEquals(Double.compare(validatedValue, 70.6458763d), 0);
     }
 
     @Test
     public void testValidateString() {
-        String validatedValue = AnyTypeJsonSchema.validate("a", configuration);
+        String validatedValue = schema.validate("a", configuration);
         Assert.assertEquals(validatedValue, "a");
     }
 
     @Test
     public void testValidateZonedDateTime() {
-        String validatedValue = AnyTypeJsonSchema.validate(ZonedDateTime.of(2017, 7, 21, 17, 32, 28, 0, ZoneId.of("Z")), configuration);
+        String validatedValue = schema.validate(ZonedDateTime.of(2017, 7, 21, 17, 32, 28, 0, ZoneId.of("Z")), configuration);
         Assert.assertEquals(validatedValue, "2017-07-21T17:32:28Z");
     }
 
     @Test
     public void testValidateLocalDate() {
-        String validatedValue = AnyTypeJsonSchema.validate(LocalDate.of(2017, 7, 21), configuration);
+        String validatedValue = schema.validate(LocalDate.of(2017, 7, 21), configuration);
         Assert.assertEquals(validatedValue, "2017-07-21");
     }
 
@@ -79,7 +80,7 @@ public class AnyTypeSchemaTest {
     public void testValidateMap() {
         LinkedHashMap<String, Object> inMap = new LinkedHashMap<>();
         inMap.put("today", LocalDate.of(2017, 7, 21));
-        FrozenMap<String, Object> validatedValue = AnyTypeJsonSchema.validate(inMap, configuration);
+        FrozenMap<String, Object> validatedValue = schema.validate(inMap, configuration);
         LinkedHashMap<String, Object> outMap = new LinkedHashMap<>();
         outMap.put("today", "2017-07-21");
         Assert.assertEquals(validatedValue, outMap);
@@ -89,7 +90,7 @@ public class AnyTypeSchemaTest {
     public void testValidateList() {
         ArrayList<Object> inList = new ArrayList<>();
         inList.add(LocalDate.of(2017, 7, 21));
-        FrozenList<Object> validatedValue = AnyTypeJsonSchema.validate(inList, configuration);
+        FrozenList<Object> validatedValue = schema.validate(inList, configuration);
         ArrayList<Object> outList = new ArrayList<>();
         outList.add( "2017-07-21");
         Assert.assertEquals(validatedValue, outList);
