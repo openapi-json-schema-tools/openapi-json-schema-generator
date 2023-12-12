fcd224ca9b74b365d5cf8602c1f0cdd4b293855d
Justin Black <justin.a.black@gmail.com>
2023-12-12 12:46:43 -0800

    Fixes UuidJsonSchema

--- samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/UuidJsonSchema.java
+++ samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/UuidJsonSchema.java
@@ -1,20 +1,20 @@
 package org.openapijsonschematools.client.schemas;
 
-import org.openapijsonschematools.client.schemas.validation.NonCollectionJsonSchema;
-import org.openapijsonschematools.client.schemas.validation.FrozenMap;
-import org.openapijsonschematools.client.schemas.validation.FrozenList;
+import org.openapijsonschematools.client.schemas.validation.JsonSchema;
 import org.openapijsonschematools.client.configurations.SchemaConfiguration;
 import org.openapijsonschematools.client.schemas.validation.KeywordEntry;
+import org.openapijsonschematools.client.schemas.validation.PathToSchemasMap;
 import org.openapijsonschematools.client.schemas.validation.TypeValidator;
 import org.openapijsonschematools.client.schemas.validation.FormatValidator;
 import org.openapijsonschematools.client.exceptions.ValidationException;
 
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
-public class UuidJsonSchema extends NonCollectionJsonSchema {
+public class UuidJsonSchema extends JsonSchema<String, String, String> {
     public UuidJsonSchema() {
         super(new LinkedHashMap<>(Map.ofEntries(
                 new KeywordEntry("type", new TypeValidator(Set.of(String.class))),
@@ -22,11 +22,17 @@ public class UuidJsonSchema extends NonCollectionJsonSchema {
         )));
     }
 
-    public String validate(String arg, SchemaConfiguration configuration) throws ValidationException {
-        return validateString(arg, configuration);
+    @Override
+    protected String castToAllowedTypes(String arg, List<Object> pathToItem, Set<List<Object>> pathSet) {
+        return castToAllowedStringTypes(arg, pathToItem, pathSet);
+    }
+
+    @Override
+    protected String getNewInstance(String arg, List<Object> pathToItem, PathToSchemasMap pathToSchemas) {
+        return arg;
     }
 
     public String validate(UUID arg, SchemaConfiguration configuration) throws ValidationException {
-        return validateUUID(arg, configuration);
+        return validate(arg.toString(), configuration);
     }
 }
