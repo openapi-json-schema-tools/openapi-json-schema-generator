379408a9083d88f091eb7c3d4b0176f1ebe1bc22
Justin Black <justin.a.black@gmail.com>
2023-12-12 11:56:14 -0800

    Updates AnyType and NotAnyType schemas

--- samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/NotAnyTypeJsonSchema.java
+++ samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/NotAnyTypeJsonSchema.java
@@ -2,7 +2,7 @@ package org.openapijsonschematools.client.schemas;
 
 import org.openapijsonschematools.client.configurations.SchemaConfiguration;
 import org.openapijsonschematools.client.exceptions.ValidationException;
-import org.openapijsonschematools.client.schemas.validation.NonCollectionJsonSchema;
+import org.openapijsonschematools.client.schemas.validation.JsonSchema;
 import org.openapijsonschematools.client.schemas.validation.FrozenMap;
 import org.openapijsonschematools.client.schemas.validation.FrozenList;
 import org.openapijsonschematools.client.schemas.validation.KeywordEntry;
@@ -11,14 +11,20 @@ import org.openapijsonschematools.client.schemas.validation.NotValidator;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class NotAnyTypeJsonSchema extends NonCollectionJsonSchema {
+public class NotAnyTypeJsonSchema extends JsonSchema<Object, Object, Object> {
     public NotAnyTypeJsonSchema() {
         super(new LinkedHashMap<>(Map.ofEntries(
                 new KeywordEntry("not", new NotValidator(AnyTypeJsonSchema.class))
         )));
     }
 
-    public Object validate(Object arg, SchemaConfiguration configuration) throws ValidationException {
-        return validate(arg, configuration);
+    @Override
+    protected Object castToAllowedTypes(Object arg, List<Object> pathToItem, Set<List<Object>> pathSet) {
+        return castToAllowedObjectTypes(arg, pathToItem, pathSet);
+    }
+
+    @Override
+    protected Object getNewInstance(Object arg, List<Object> pathToItem, PathToSchemasMap pathToSchemas) {
+        return arg;
     }
 }
