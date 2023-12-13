d9598dc0e967d39818abf59b0e45be6f53bcc16c
Justin Black <justin.a.black@gmail.com>
2023-12-12 21:58:25 -0800

    Fixes validate method for integer types

--- samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/components/schemas/InvalidInstanceShouldNotRaiseErrorWhenFloatDivisionInf.java
+++ samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/components/schemas/InvalidInstanceShouldNotRaiseErrorWhenFloatDivisionInf.java
@@ -50,20 +50,31 @@ public class InvalidInstanceShouldNotRaiseErrorWhenFloatDivisionInf {
             return arg;
         }
     
+        @Override
+        public Number validate(Number arg, SchemaConfiguration configuration) throws ValidationException {
+            Set<List<Object>> pathSet = new HashSet<>();
+            List<Object> pathToItem = List.of("args[0");
+            Number castArg = castToAllowedTypes(arg, pathToItem, pathSet);
+            SchemaConfiguration usedConfiguration = Objects.requireNonNullElseGet(configuration, () -> new SchemaConfiguration(JsonSchemaKeywordFlags.ofNone()));
+            ValidationMetadata validationMetadata = new ValidationMetadata(pathToItem, usedConfiguration, new PathToSchemasMap(), new LinkedHashSet<>());
+            PathToSchemasMap pathToSchemasMap = getPathToSchemas(this, castArg, validationMetadata, pathSet);
+            return getNewInstance(castArg, validationMetadata.pathToItem(), pathToSchemasMap);
+        }
+        
         public int validate(int arg, SchemaConfiguration configuration) throws ValidationException {
-            return validateInt(arg, configuration);
+            return (int) validate((Number) arg, configuration);
         }
         
         public float validate(float arg, SchemaConfiguration configuration) throws ValidationException {
-            return validateFloat(arg, configuration);
+            return (float) validate((Number) arg, configuration);
         }
         
         public long validate(long arg, SchemaConfiguration configuration) throws ValidationException {
-            return validateLong(arg, configuration);
+            return (long) validate((Number) arg, configuration);
         }
         
         public double validate(double arg, SchemaConfiguration configuration) throws ValidationException {
-            return validateDouble(arg, configuration);
+            return (double) validate((Number) arg, configuration);
         }
     }
 }
