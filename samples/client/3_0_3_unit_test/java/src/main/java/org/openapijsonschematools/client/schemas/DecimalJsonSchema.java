b669371869742c393b19b0ee7e407993555e1fb8
Justin Black <justin.a.black@gmail.com>
2023-12-12 18:42:52 -0800

    Updates DecimalJsonSchema

--- samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/DecimalJsonSchema.java
+++ samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/DecimalJsonSchema.java
@@ -1,17 +1,26 @@
 package org.openapijsonschematools.client.schemas;
 
+import org.openapijsonschematools.client.configurations.JsonSchemaKeywordFlags;
+import org.openapijsonschematools.client.configurations.SchemaConfiguration;
+import org.openapijsonschematools.client.exceptions.InvalidTypeException;
+import org.openapijsonschematools.client.exceptions.ValidationException;
 import org.openapijsonschematools.client.schemas.validation.JsonSchema;
 import org.openapijsonschematools.client.schemas.validation.KeywordEntry;
 import org.openapijsonschematools.client.schemas.validation.PathToSchemasMap;
+import org.openapijsonschematools.client.schemas.validation.SchemaStringValidator;
 import org.openapijsonschematools.client.schemas.validation.TypeValidator;
 import org.openapijsonschematools.client.schemas.validation.FormatValidator;
+import org.openapijsonschematools.client.schemas.validation.ValidationMetadata;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
-public class DecimalJsonSchema extends JsonSchema<String, String, String> {
+public class DecimalJsonSchema extends JsonSchema implements SchemaStringValidator {
     public DecimalJsonSchema() {
         super(new LinkedHashMap<>(Map.ofEntries(
                 new KeywordEntry("type", new TypeValidator(Set.of(String.class))),
@@ -20,12 +29,23 @@ public class DecimalJsonSchema extends JsonSchema<String, String, String> {
     }
 
     @Override
-    protected String castToAllowedTypes(String arg, List<Object> pathToItem, Set<List<Object>> pathSet) {
+    public String castToAllowedTypes(String arg, List<Object> pathToItem, Set<List<Object>> pathSet) {
         return castToAllowedStringTypes(arg, pathToItem, pathSet);
     }
 
     @Override
-    protected String getNewInstance(String arg, List<Object> pathToItem, PathToSchemasMap pathToSchemas) {
+    public String getNewInstance(String arg, List<Object> pathToItem, PathToSchemasMap pathToSchemas) {
         return arg;
+    }
+
+    @Override
+    public String validate(String arg, SchemaConfiguration configuration) throws ValidationException, InvalidTypeException {
+        Set<List<Object>> pathSet = new HashSet<>();
+        List<Object> pathToItem = List.of("args[0");
+        String castArg = castToAllowedTypes(arg, pathToItem, pathSet);
+        SchemaConfiguration usedConfiguration = Objects.requireNonNullElseGet(configuration, () -> new SchemaConfiguration(JsonSchemaKeywordFlags.ofNone()));
+        ValidationMetadata validationMetadata = new ValidationMetadata(pathToItem, usedConfiguration, new PathToSchemasMap(), new LinkedHashSet<>());
+        PathToSchemasMap pathToSchemasMap = getPathToSchemas(this, castArg, validationMetadata, pathSet);
+        return getNewInstance(castArg, validationMetadata.pathToItem(), pathToSchemasMap);
     }
 }
