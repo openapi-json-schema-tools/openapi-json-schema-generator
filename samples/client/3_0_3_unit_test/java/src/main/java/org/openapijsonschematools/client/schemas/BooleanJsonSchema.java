b6df2ccf7c034faa1ef7418fe97ae3c7967f99b0
Justin Black <justin.a.black@gmail.com>
2023-12-12 18:22:18 -0800

    Updates BooleanJsonSchema

--- samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/BooleanJsonSchema.java
+++ samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/BooleanJsonSchema.java
@@ -1,16 +1,26 @@
 package org.openapijsonschematools.client.schemas;
 
+import org.openapijsonschematools.client.configurations.JsonSchemaKeywordFlags;
+import org.openapijsonschematools.client.configurations.SchemaConfiguration;
+import org.openapijsonschematools.client.exceptions.InvalidTypeException;
+import org.openapijsonschematools.client.exceptions.ValidationException;
 import org.openapijsonschematools.client.schemas.validation.JsonSchema;
 import org.openapijsonschematools.client.schemas.validation.KeywordEntry;
 import org.openapijsonschematools.client.schemas.validation.PathToSchemasMap;
+import org.openapijsonschematools.client.schemas.validation.SchemaBooleanValidator;
 import org.openapijsonschematools.client.schemas.validation.TypeValidator;
+import org.openapijsonschematools.client.schemas.validation.ValidationMetadata;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
-public class BooleanJsonSchema extends JsonSchema<Boolean, Boolean, Boolean> {
+public class BooleanJsonSchema extends JsonSchema implements SchemaBooleanValidator {
     public BooleanJsonSchema() {
         super(new LinkedHashMap<>(Map.ofEntries(
                 new KeywordEntry("type", new TypeValidator(Set.of(Boolean.class)))
@@ -18,12 +28,25 @@ public class BooleanJsonSchema extends JsonSchema<Boolean, Boolean, Boolean> {
     }
 
     @Override
-    protected Boolean castToAllowedTypes(Boolean arg, List<Object> pathToItem, Set<List<Object>> pathSet) {
+    public boolean castToAllowedTypes(boolean arg, List<Object> pathToItem, Set<List<Object>> pathSet) {
         return castToAllowedBooleanTypes(arg, pathToItem, pathSet);
     }
 
     @Override
-    protected Boolean getNewInstance(Boolean arg, List<Object> pathToItem, PathToSchemasMap pathToSchemas) {
+    public boolean getNewInstance(boolean arg, List<Object> pathToItem, PathToSchemasMap pathToSchemas) {
         return arg;
+    }
+
+    @Override
+    public boolean validate(boolean arg, SchemaConfiguration configuration) throws ValidationException, InvalidTypeException {
+        Set<List<Object>> pathSet = new HashSet<>();
+        List<Object> pathToItem = new ArrayList<>();
+        pathToItem.add("args[0]");
+        boolean castArg = castToAllowedTypes(arg, pathToItem, pathSet);
+        SchemaConfiguration usedConfiguration = Objects.requireNonNullElseGet(configuration, () -> new SchemaConfiguration(JsonSchemaKeywordFlags.ofNone()));
+        PathToSchemasMap validatedPathToSchemas = new PathToSchemasMap();
+        ValidationMetadata validationMetadata = new ValidationMetadata(pathToItem, usedConfiguration, validatedPathToSchemas, new LinkedHashSet<>());
+        PathToSchemasMap pathToSchemasMap = getPathToSchemas(this, castArg, validationMetadata, pathSet);
+        return getNewInstance(castArg, validationMetadata.pathToItem(), pathToSchemasMap);
     }
 }
