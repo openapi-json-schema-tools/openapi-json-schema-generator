86cdf1dacc44c338d6006056eca2414e3b391bfa
Justin Black <justin.a.black@gmail.com>
2023-12-12 19:12:58 -0800

    Updates FloatJsonSchema

--- samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/FloatJsonSchema.java
+++ samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/FloatJsonSchema.java
@@ -1,17 +1,26 @@
 package org.openapijsonschematools.client.schemas;
 
+import org.openapijsonschematools.client.configurations.JsonSchemaKeywordFlags;
+import org.openapijsonschematools.client.configurations.SchemaConfiguration;
+import org.openapijsonschematools.client.exceptions.InvalidTypeException;
+import org.openapijsonschematools.client.exceptions.ValidationException;
 import org.openapijsonschematools.client.schemas.validation.JsonSchema;
 import org.openapijsonschematools.client.schemas.validation.KeywordEntry;
 import org.openapijsonschematools.client.schemas.validation.PathToSchemasMap;
+import org.openapijsonschematools.client.schemas.validation.SchemaNumberValidator;
 import org.openapijsonschematools.client.schemas.validation.TypeValidator;
 import org.openapijsonschematools.client.schemas.validation.FormatValidator;
+import org.openapijsonschematools.client.schemas.validation.ValidationMetadata;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
-public class FloatJsonSchema extends JsonSchema<Float, Number, Float> {
+public class FloatJsonSchema extends JsonSchema implements SchemaNumberValidator {
     public FloatJsonSchema() {
         super(new LinkedHashMap<>(Map.ofEntries(
                 new KeywordEntry("type", new TypeValidator(Set.of(Float.class))),
@@ -20,12 +29,27 @@ public class FloatJsonSchema extends JsonSchema<Float, Number, Float> {
     }
 
     @Override
-    protected Number castToAllowedTypes(Float arg, List<Object> pathToItem, Set<List<Object>> pathSet) {
+    public Number castToAllowedTypes(Number arg, List<Object> pathToItem, Set<List<Object>> pathSet) {
         return castToAllowedNumberTypes(arg, pathToItem, pathSet);
     }
 
     @Override
-    protected Float getNewInstance(Number arg, List<Object> pathToItem, PathToSchemasMap pathToSchemas) {
-        return (Float) arg;
+    public Number getNewInstance(Number arg, List<Object> pathToItem, PathToSchemasMap pathToSchemas) {
+        return arg;
+    }
+
+    @Override
+    public Number validate(Number arg, SchemaConfiguration configuration) throws ValidationException, InvalidTypeException {
+        Set<List<Object>> pathSet = new HashSet<>();
+        List<Object> pathToItem = List.of("args[0");
+        Number castArg = castToAllowedTypes(arg, pathToItem, pathSet);
+        SchemaConfiguration usedConfiguration = Objects.requireNonNullElseGet(configuration, () -> new SchemaConfiguration(JsonSchemaKeywordFlags.ofNone()));
+        ValidationMetadata validationMetadata = new ValidationMetadata(pathToItem, usedConfiguration, new PathToSchemasMap(), new LinkedHashSet<>());
+        PathToSchemasMap pathToSchemasMap = getPathToSchemas(this, castArg, validationMetadata, pathSet);
+        return getNewInstance(castArg, validationMetadata.pathToItem(), pathToSchemasMap);
+    }
+
+    public float validate(float arg, SchemaConfiguration configuration) {
+        return (float) validate((Number) arg, configuration);
     }
 }
