23e42b06045b094de801bf684aec7fcf7fafd439
Justin Black <justin.a.black@gmail.com>
2023-12-12 20:46:56 -0800

    Fixes MapSchemaTest

--- samples/client/3_0_3_unit_test/java/src/test/java/org/openapijsonschematools/client/schemas/MapSchemaTest.java
+++ samples/client/3_0_3_unit_test/java/src/test/java/org/openapijsonschematools/client/schemas/MapSchemaTest.java
@@ -8,19 +8,31 @@ import org.openapijsonschematools.client.schemas.validation.JsonSchema;
 import org.openapijsonschematools.client.schemas.validation.JsonSchemaFactory;
 import org.openapijsonschematools.client.schemas.validation.FrozenMap;
 import org.openapijsonschematools.client.exceptions.ValidationException;
+import org.openapijsonschematools.client.schemas.validation.PathToSchemasMap;
+import org.openapijsonschematools.client.schemas.validation.ValidationMetadata;
 
 import java.time.LocalDate;
 import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Map;
 
 public class MapSchemaTest {
     static final SchemaConfiguration configuration = new SchemaConfiguration(JsonSchemaKeywordFlags.ofNone());
     static final MapJsonSchema mapJsonSchema = JsonSchemaFactory.getInstance(MapJsonSchema.class);
+    static final ValidationMetadata validationMetadata = new ValidationMetadata(
+            List.of("args[0"),
+            configuration,
+            new PathToSchemasMap(),
+            new LinkedHashSet<>()
+    );
 
     @Test
     public void testExceptionThrownForInvalidType() {
-        Assert.assertThrows(ValidationException.class, () -> mapJsonSchema.validate(
-                (Void) null, configuration
+        Assert.assertThrows(ValidationException.class, () -> JsonSchema.validate(
+                mapJsonSchema,
+                null,
+                validationMetadata
         ));
     }
 
@@ -28,7 +40,7 @@ public class MapSchemaTest {
     public void testValidateMap() {
         Map<String, Object> inMap = new LinkedHashMap<>();
         inMap.put("today", LocalDate.of(2017, 7, 21));
-        FrozenMap<String, Object> validatedValue = mapJsonSchema.validate(inMap, configuration);
+        FrozenMap<Object> validatedValue = mapJsonSchema.validate(inMap, configuration);
         LinkedHashMap<String, String> outMap = new LinkedHashMap<>();
         outMap.put("today", "2017-07-21");
         Assert.assertEquals(validatedValue, outMap);
