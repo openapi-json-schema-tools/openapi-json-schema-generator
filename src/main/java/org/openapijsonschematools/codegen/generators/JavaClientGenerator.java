73176b6c2e1066edcaeedf3d21f2b17fec63039e
Justin Black <justin.a.black@gmail.com>
2024-01-05 11:06:05 -0800

    Removes supportStreaming

--- src/main/java/org/openapijsonschematools/codegen/generators/JavaClientGenerator.java
+++ src/main/java/org/openapijsonschematools/codegen/generators/JavaClientGenerator.java
@@ -72,7 +72,6 @@ public class JavaClientGenerator extends AbstractJavaGenerator
     public static final String USE_RUNTIME_EXCEPTION = "useRuntimeException";
     public static final String USE_REFLECTION_EQUALS_HASHCODE = "useReflectionEqualsHashCode";
     public static final String USE_ABSTRACTION_FOR_FILES = "useAbstractionForFiles";
-    public static final String SUPPORT_STREAMING = "supportStreaming";
     public static final String FEIGN = "feign";
     public static final String GOOGLE_API_CLIENT = "google-api-client";
     public static final String JERSEY1 = "jersey1";
@@ -107,7 +106,6 @@ public class JavaClientGenerator extends AbstractJavaGenerator
     protected boolean useRuntimeException = false;
     protected boolean useReflectionEqualsHashCode = false;
     protected boolean useAbstractionForFiles = false;
-    protected boolean supportStreaming = false;
     protected String authFolder;
     protected boolean useOneOfDiscriminatorLookup = false; // use oneOf discriminator's mapping for model lookup
     protected String rootJavaEEPackage;
@@ -233,7 +231,6 @@ public class JavaClientGenerator extends AbstractJavaGenerator
         cliOptions.add(CliOption.newBoolean(USE_RUNTIME_EXCEPTION, "Use RuntimeException instead of Exception"));
         cliOptions.add(CliOption.newBoolean(USE_REFLECTION_EQUALS_HASHCODE, "Use org.apache.commons.lang3.builder for equals and hashCode in the models. WARNING: This will fail under a security manager, unless the appropriate permissions are set up correctly and also there's potential performance impact."));
         cliOptions.add(CliOption.newBoolean(USE_ABSTRACTION_FOR_FILES, "Use alternative types instead of java.io.File to allow passing bytes without a file on disk. Available on resttemplate, webclient, libraries"));
-        cliOptions.add(CliOption.newBoolean(SUPPORT_STREAMING, "Support streaming endpoint (beta)", this.supportStreaming));
         cliOptions.add(CliOption.newBoolean(CodegenConstants.USE_ONEOF_DISCRIMINATOR_LOOKUP, CodegenConstants.USE_ONEOF_DISCRIMINATOR_LOOKUP_DESC + " Only jersey2, jersey3, native, okhttp-gson support this option."));
         cliOptions.add(CliOption.newBoolean(CodegenConstants.USE_SINGLE_REQUEST_PARAMETER, "Setting this property to true will generate functions with a single argument containing all API endpoint parameters instead of one argument per parameter. ONLY jersey2, jersey3, okhttp-gson support this option."));
 
@@ -535,11 +532,6 @@ public class JavaClientGenerator extends AbstractJavaGenerator
             this.setUseAbstractionForFiles(convertPropertyToBooleanAndWriteBack(USE_ABSTRACTION_FOR_FILES));
         }
 
-        if (additionalProperties.containsKey(SUPPORT_STREAMING)) {
-            this.setSupportStreaming(Boolean.parseBoolean(additionalProperties.get(SUPPORT_STREAMING).toString()));
-        }
-        additionalProperties.put(SUPPORT_STREAMING, supportStreaming);
-
         final String invokerFolder = (sourceFolder + '/' + invokerPackage).replace(".", "/");
         final String apiFolder = (sourceFolder + '/' + apiPackage).replace(".", "/");
         final String modelsFolder = (sourceFolder + File.separator + modelPackage().replace('.', File.separatorChar)).replace('/', File.separatorChar);
@@ -704,11 +696,7 @@ public class JavaClientGenerator extends AbstractJavaGenerator
     public void setUseAbstractionForFiles(boolean useAbstractionForFiles) {
         this.useAbstractionForFiles = useAbstractionForFiles;
     }
-
-    public void setSupportStreaming(final boolean supportStreaming) {
-        this.supportStreaming = supportStreaming;
-    }
-
+    
     @Override
     public Map<String, Object> postProcessSupportingFileData(Map<String, Object> data) {
         generateYAMLSpecFile(data);
