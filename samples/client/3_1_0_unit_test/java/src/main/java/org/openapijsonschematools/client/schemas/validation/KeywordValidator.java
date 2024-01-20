ccf815561cdd68d6210f988657ed52ff781f1916
Justin Black <justin.a.black@gmail.com>
2024-01-19 20:45:33 -0800

    Adds functionalinterface annotation

--- samples/client/3_1_0_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/validation/KeywordValidator.java
+++ samples/client/3_1_0_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/validation/KeywordValidator.java
@@ -3,6 +3,7 @@ package org.openapijsonschematools.client.schemas.validation;
 import org.openapijsonschematools.client.exceptions.ValidationException;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
+@FunctionalInterface
 public interface KeywordValidator {
     @Nullable PathToSchemasMap validate(
         ValidationData data
