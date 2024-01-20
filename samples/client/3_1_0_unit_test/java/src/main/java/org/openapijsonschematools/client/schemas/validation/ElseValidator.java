c243e7982179ba844702df1cfe871c5a51645ffe
Justin Black <justin.a.black@gmail.com>
2024-01-20 10:10:17 -0800

    Adds else validator implementation

--- samples/client/3_1_0_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/validation/ElseValidator.java
+++ samples/client/3_1_0_unit_test/java/src/main/java/org/openapijsonschematools/client/schemas/validation/ElseValidator.java
@@ -1,6 +1,7 @@
 package org.openapijsonschematools.client.schemas.validation;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.openapijsonschematools.client.exceptions.InvalidTypeException;
 import org.openapijsonschematools.client.exceptions.ValidationException;
 
 public class ElseValidator implements KeywordValidator {
@@ -8,7 +9,25 @@ public class ElseValidator implements KeywordValidator {
     public @Nullable PathToSchemasMap validate(
         ValidationData data
     ) {
-        // todo
-        return null;
+        var elseSchema = data.schema().elseSchema;
+        if (elseSchema == null) {
+            return null;
+        }
+        var ifPathToSchemas = data.ifPathToSchemas();
+        if (ifPathToSchemas == null) {
+            // if unset
+            return null;
+        }
+        if (!ifPathToSchemas.isEmpty()) {
+            // if validation is true
+            return null;
+        }
+        JsonSchema elseSchemaInstance = JsonSchemaFactory.getInstance(elseSchema);
+        PathToSchemasMap pathToSchemas = new PathToSchemasMap();
+        try {
+            var elsePathToSchemas = JsonSchema.validate(elseSchemaInstance, data.arg(), data.validationMetadata());
+            pathToSchemas.update(elsePathToSchemas);
+        } catch (ValidationException | InvalidTypeException ignored) {}
+        return pathToSchemas;
     }
 }
