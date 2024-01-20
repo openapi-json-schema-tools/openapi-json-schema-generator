db846068aba6b73e088d5a51e044040b0bc12daf
Justin Black <justin.a.black@gmail.com>
2024-01-19 19:26:43 -0800

    Updates RequiredValidatorTest

--- samples/client/3_1_0_unit_test/java/src/test/java/org/openapijsonschematools/client/schemas/validation/RequiredValidatorTest.java
+++ samples/client/3_1_0_unit_test/java/src/test/java/org/openapijsonschematools/client/schemas/validation/RequiredValidatorTest.java
@@ -5,16 +5,42 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openapijsonschematools.client.configurations.JsonSchemaKeywordFlags;
 import org.openapijsonschematools.client.configurations.SchemaConfiguration;
+import org.openapijsonschematools.client.exceptions.InvalidTypeException;
 import org.openapijsonschematools.client.exceptions.ValidationException;
-import org.openapijsonschematools.client.schemas.MapJsonSchema;
 
-import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class RequiredValidatorTest {
+    public static class ObjectWithRequiredSchema extends JsonSchema {
+        private ObjectWithRequiredSchema() {
+            super(new JsonSchemaInfo()
+                    .type(Set.of(Map.class))
+                    .required(Set.of("someString"))
+            );
+
+        }
+
+        @Override
+        public Object getNewInstance(@Nullable Object arg, List<Object> pathToItem, PathToSchemasMap pathToSchemas) {
+            if (arg instanceof Map<?, ?> mapArg) {
+                return getNewInstance(mapArg, pathToItem, pathToSchemas);
+            }
+            throw new InvalidTypeException("Invalid input type="+getClass(arg)+". It can't be instantiated by this schema");
+        }
+
+        @Override
+        public @Nullable Object validate(@Nullable Object arg, SchemaConfiguration configuration) throws InvalidTypeException, ValidationException {
+            if (arg instanceof Map<?, ?> mapArg) {
+                return validate(mapArg, configuration);
+            }
+            throw new InvalidTypeException("Invalid input type="+getClass(arg)+". It can't be validated by this schema");
+        }
+    }
+
     @SuppressWarnings("nullness")
     private void assertNull(@Nullable Object object) {
         Assert.assertNull(object);
@@ -22,9 +48,6 @@ public class RequiredValidatorTest {
 
     @Test
     public void testCorrectPropertySucceeds() {
-        Set<String> requiredProperties = new LinkedHashSet<>();
-        requiredProperties.add("someString");
-
         List<Object> pathToItem = List.of("args[0]");
         ValidationMetadata validationMetadata = new ValidationMetadata(
                 pathToItem,
@@ -35,15 +58,12 @@ public class RequiredValidatorTest {
         LinkedHashMap<String, Object> mutableMap = new LinkedHashMap<>();
         mutableMap.put("someString", "abc");
         FrozenMap<Object> arg = new FrozenMap<>(mutableMap);
-        final RequiredValidator validator = new RequiredValidator(requiredProperties);
+        final RequiredValidator validator = new RequiredValidator();
         PathToSchemasMap pathToSchemas = validator.validate(
                 new ValidationData(
-                    MapJsonSchema.getInstance(),
+                    new ObjectWithRequiredSchema(),
                     arg,
-                    validationMetadata,
-                    null,
-                    null,
-                    null
+                    validationMetadata
                 )
         );
         assertNull(pathToSchemas);
@@ -51,9 +71,6 @@ public class RequiredValidatorTest {
 
     @Test
     public void testNotApplicableTypeReturnsNull() {
-        Set<String> requiredProperties = new LinkedHashSet<>();
-        requiredProperties.add("someString");
-
         List<Object> pathToItem = List.of("args[0]");
         ValidationMetadata validationMetadata = new ValidationMetadata(
                 pathToItem,
@@ -61,15 +78,12 @@ public class RequiredValidatorTest {
                 new PathToSchemasMap(),
                 new LinkedHashSet<>()
         );
-        final RequiredValidator validator = new RequiredValidator(requiredProperties);
+        final RequiredValidator validator = new RequiredValidator();
         PathToSchemasMap pathToSchemas = validator.validate(
                 new ValidationData(
-                    MapJsonSchema.getInstance(),
+                    new ObjectWithRequiredSchema(),
                     1,
-                    validationMetadata,
-                    null,
-                    null,
-                    null
+                    validationMetadata
                 )
         );
         assertNull(pathToSchemas);
@@ -77,9 +91,6 @@ public class RequiredValidatorTest {
 
     @Test
     public void testIncorrectPropertyFails() {
-        Set<String> requiredProperties = new LinkedHashSet<>();
-        requiredProperties.add("someString");
-
         List<Object> pathToItem = List.of("args[0]");
         ValidationMetadata validationMetadata = new ValidationMetadata(
                 pathToItem,
@@ -90,15 +101,12 @@ public class RequiredValidatorTest {
         LinkedHashMap<String, Object> mutableMap = new LinkedHashMap<>();
         mutableMap.put("aDifferentProp", 1);
         FrozenMap<Object> arg = new FrozenMap<>(mutableMap);
-        final RequiredValidator validator = new RequiredValidator(requiredProperties);
+        final RequiredValidator validator = new RequiredValidator();
         Assert.assertThrows(ValidationException.class, () -> validator.validate(
                 new ValidationData(
-                    MapJsonSchema.getInstance(),
+                    new ObjectWithRequiredSchema(),
                     arg,
-                    validationMetadata,
-                    null,
-                    null,
-                    null
+                    validationMetadata
                 )
         ));
     }
