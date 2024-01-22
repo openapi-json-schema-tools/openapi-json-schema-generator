d111cf934030f03f18c7392211f845331aafaaa6
Justin Black <spacether@users.noreply.github.com>
2024-01-22 15:46:11 -0800

    Speeds up java client tests (#378)
    * Updates pom to include profile for java17clients
    
    * Adjusts packaging
    
    * Adjusts testJava17ClientSamples job
    
    * Updates path to file
    
    * Fixes filepath typo
    
    * Reverts pom

--- .circleci/parallel.sh
+++ .circleci/parallel.sh
@@ -28,10 +28,7 @@ elif [ "$JOB_ID" = "testPythonClientSamples" ]; then
 elif [ "$JOB_ID" = "testJava17ClientSamples" ]; then
   echo "Running job $JOB_ID ..."
   java -version
-
-  (cd samples/client/petstore/java && mvn test)
-  (cd samples/client/3_0_3_unit_test/java && mvn test)
-  (cd samples/client/3_1_0_unit_test/java && mvn test)
+  cat ./.circleci/testJava17ClientSamples.sh | parallel
 
 else
   echo "Running job $JOB_ID"
