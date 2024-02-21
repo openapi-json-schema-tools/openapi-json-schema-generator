85f5a6d3ac23eea690cefdd4b03db6f4bb061fb9
Justin Black <justin.a.black@gmail.com>
2024-02-21 12:18:28 -0800

    Fixes handling or responses with no schema definition

--- samples/client/petstore/java/src/main/java/org/openapijsonschematools/client/paths/fakeresponsewithoutschema/get/responses/Code200Response.java
+++ samples/client/petstore/java/src/main/java/org/openapijsonschematools/client/paths/fakeresponsewithoutschema/get/responses/Code200Response.java
@@ -11,49 +11,20 @@ import java.util.Map;
 import java.net.http.HttpHeaders;
 
 public class Code200Response {
-    public sealed interface SealedMediaType permits ApplicationjsonMediaType, ApplicationxmlMediaType {}
 
-    public record ApplicationjsonMediaType() implements SealedMediaType, MediaType<> {
-        public ApplicationjsonMediaType() {
-            this();
-        }
-        @Override
-        public Void encoding() {
-            return null;
-        }
-    }
-
-    public record ApplicationxmlMediaType() implements SealedMediaType, MediaType<> {
-        public ApplicationxmlMediaType() {
-            this();
-        }
-        @Override
-        public Void encoding() {
-            return null;
-        }
-    }
-    public sealed interface SealedResponseBody permits ApplicationjsonResponseBody, ApplicationxmlResponseBody {}
-    public record ApplicationjsonResponseBody( body) implements SealedResponseBody { }
-    public record ApplicationxmlResponseBody( body) implements SealedResponseBody { }
-
-    public static class Code200Response1 extends ResponseDeserializer<SealedResponseBody, Void, SealedMediaType> {
+    public static class Code200Response1 extends ResponseDeserializer<Void, Void, Void> {
         public Code200Response1() {
             super(
                 Map.ofEntries(
-                    new AbstractMap.SimpleEntry<>("application/json", new ApplicationjsonMediaType()),
-                    new AbstractMap.SimpleEntry<>("application/xml", new ApplicationxmlMediaType())
+                    new AbstractMap.SimpleEntry<>("application/json", null),
+                    new AbstractMap.SimpleEntry<>("application/xml", null)
                 )
             );
         }
 
         @Override
-        public SealedResponseBody getBody(String contentType, byte[] body, SchemaConfiguration configuration) {
-            if ("application/json".equals(contentType)) {
-                // todo implement deserialization
-            } else if ("application/xml".equals(contentType)) {
-                // todo implement deserialization
-            }
-            throw new RuntimeException("contentType="+contentType+" returned by the server is unknown and does not exist in the openapi document");
+        public Void getBody(String contentType, byte[] body, SchemaConfiguration configuration) {
+            return null;
         }
 
         @Override
