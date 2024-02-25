abf0a553dd734bef626aa29ac535f869ddf26173
Justin Black <justin.a.black@gmail.com>
2024-02-25 09:29:59 -0800

    Samples updated, maven-surefire-plugin version updated

--- .circleci/testJava17ClientSamples.sh
+++ .circleci/testJava17ClientSamples.sh
@@ -1,3 +1,3 @@
-  (cd samples/client/petstore/java && mvn test -e -X)
-  (cd samples/client/3_0_3_unit_test/java && mvn test -e -X)
-  (cd samples/client/3_1_0_unit_test/java && mvn test -e -X)
+  (cd samples/client/petstore/java && mvn test)
+  (cd samples/client/3_0_3_unit_test/java && mvn test)
+  (cd samples/client/3_1_0_unit_test/java && mvn test)
