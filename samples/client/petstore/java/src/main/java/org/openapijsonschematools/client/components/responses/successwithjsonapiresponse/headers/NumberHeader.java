f2fb2d25c523e9db403af74fb68a89f668271e17
Justin Black <justin.a.black@gmail.com>
2024-02-28 16:07:28 -0800

    Fixes case where imported schema class was overwritten

--- samples/client/petstore/java/src/main/java/org/openapijsonschematools/client/components/responses/successwithjsonapiresponse/headers/NumberHeader.java
+++ samples/client/petstore/java/src/main/java/org/openapijsonschematools/client/components/responses/successwithjsonapiresponse/headers/NumberHeader.java
@@ -1,7 +1,3 @@
 package org.openapijsonschematools.client.components.responses.successwithjsonapiresponse.headers;
 
 import org.openapijsonschematools.client.components.headers.NumberHeader;
-
-public class NumberHeader extends NumberHeader {
-    public static class NumberHeader1 extends NumberHeader1 {}
-}
--- samples/client/petstore/java/src/main/java/org/openapijsonschematools/client/paths/userlogin/get/responses/code200response/headers/NumberHeader.java
+++ samples/client/petstore/java/src/main/java/org/openapijsonschematools/client/paths/userlogin/get/responses/code200response/headers/NumberHeader.java
@@ -1,7 +1,3 @@
 package org.openapijsonschematools.client.paths.userlogin.get.responses.code200response.headers;
 
 import org.openapijsonschematools.client.components.headers.NumberHeader;
-
-public class NumberHeader extends NumberHeader {
-    public static class NumberHeader1 extends NumberHeader1 {}
-}
