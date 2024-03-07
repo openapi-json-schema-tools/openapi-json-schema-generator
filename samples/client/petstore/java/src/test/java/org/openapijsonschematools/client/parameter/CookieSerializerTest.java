9ab260497e205a689b3d5c83d422743b8a5b18f0
Justin Black <justin.a.black@gmail.com>
2024-03-06 22:10:53 -0800

    Adjsut type signature in cookie serializer to fix error

--- samples/client/petstore/java/src/test/java/org/openapijsonschematools/client/parameter/CookieSerializerTest.java
+++ samples/client/petstore/java/src/test/java/org/openapijsonschematools/client/parameter/CookieSerializerTest.java
@@ -1,5 +1,6 @@
 package org.openapijsonschematools.client.parameter;
 
+import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.Assert;
 import org.junit.Test;
 import org.openapijsonschematools.client.schemas.AnyTypeJsonSchema;
@@ -33,7 +34,7 @@ public class CookieSerializerTest {
 
     @Test
     public void testSerialization() {
-        Map<String, ?> inData = Map.ofEntries(
+        Map<String, @Nullable Object> inData = Map.ofEntries(
                 new AbstractMap.SimpleEntry<>("param1", "a"),
                 new AbstractMap.SimpleEntry<>("param2", 3.14d)
         );
