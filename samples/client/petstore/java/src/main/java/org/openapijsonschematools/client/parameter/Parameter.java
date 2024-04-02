c7cec9bed48784c4c9ac58e6172ddef97a6c7204
Justin Black <justin.a.black@gmail.com>
2024-04-01 17:01:31 -0700

    Adds more needed excption signatures

--- samples/client/petstore/java/src/main/java/org/openapijsonschematools/client/parameter/Parameter.java
+++ samples/client/petstore/java/src/main/java/org/openapijsonschematools/client/parameter/Parameter.java
@@ -2,9 +2,10 @@ package org.openapijsonschematools.client.parameter;
 
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.openapijsonschematools.client.exceptions.NotImplementedException;
+import org.openapijsonschematools.client.exceptions.OpenapiDocumentException;
 
 import java.util.AbstractMap;
 
 public interface Parameter {
-    AbstractMap.SimpleEntry<String, String> serialize(@Nullable Object inData) throws NotImplementedException;
+    AbstractMap.SimpleEntry<String, String> serialize(@Nullable Object inData) throws NotImplementedException, OpenapiDocumentException;
 }
