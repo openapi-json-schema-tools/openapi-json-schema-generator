3a14c01db3f803e2297e7bfe750d8e91e8a81e29
Justin Black <justin.a.black@gmail.com>
2024-04-12 12:08:57 -0700

    Sample regen to remove unused java fiiles

--- samples/client/3_0_3_unit_test/java/src/main/java/org/openapijsonschematools/client/servers/Server0.java
+++
@@ -1,9 +0,0 @@
-package org.openapijsonschematools.client.servers;
-
-import org.openapijsonschematools.client.servers.ServerWithoutVariables;
-
-public class Server0 extends ServerWithoutVariables {
-    public Server0() {
-        super("https://someserver.com/v1");
-    }
-}
