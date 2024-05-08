447f1b06dc1e5644f081265aa6b46471cadd0927
Justin Black <justin.a.black@gmail.com>
2024-05-08 11:14:15 -0700

    Updates max and min items validators

--- samples/client/petstore/kotlin/src/main/kotlin/org/openapijsonschematools/client/schemas/validation/MaxItemsValidator.kt
+++ samples/client/petstore/kotlin/src/main/kotlin/org/openapijsonschematools/client/schemas/validation/MaxItemsValidator.kt
@@ -1,25 +1,19 @@
-package org.openapijsonschematools.client.schemas.validation;
+package org.openapijsonschematools.client.schemas.validation
 
-import org.openapijsonschematools.client.exceptions.ValidationException;
-import org.checkerframework.checker.nullness.qual.Nullable;
+import org.openapijsonschematools.client.exceptions.ValidationException
 
-import java.util.List;
-
-public class MaxItemsValidator implements KeywordValidator {
-    @Override
-    public @Nullable PathToSchemasMap validate(
-        ValidationData data
-    ) throws ValidationException {
-        var maxItems = data.schema().maxItems;
-        if (maxItems == null) {
-            return null;
+class MaxItemsValidator : KeywordValidator {
+    @Throws(ValidationException::class)
+    override fun validate(
+        data: ValidationData
+    ): PathToSchemasMap? {
+        val maxItems: Int = data.schema.maxItems ?: return null
+        if (data.arg !is List<*>) {
+            return null
         }
-        if (!(data.arg() instanceof List<?> listArg)) {
-            return null;
+        if (data.arg.size > maxItems) {
+            throw ValidationException("Value ${data.arg} is invalid because has > the maxItems of $maxItems")
         }
-        if (listArg.size() > maxItems) {
-            throw new ValidationException("Value " + listArg + " is invalid because has > the maxItems of " + maxItems);
-        }
-        return null;
+        return null
     }
 }
--- samples/client/petstore/kotlin/src/main/kotlin/org/openapijsonschematools/client/schemas/validation/MinItemsValidator.kt
+++ samples/client/petstore/kotlin/src/main/kotlin/org/openapijsonschematools/client/schemas/validation/MinItemsValidator.kt
@@ -1,25 +1,19 @@
-package org.openapijsonschematools.client.schemas.validation;
+package org.openapijsonschematools.client.schemas.validation
 
-import org.openapijsonschematools.client.exceptions.ValidationException;
-import org.checkerframework.checker.nullness.qual.Nullable;
+import org.openapijsonschematools.client.exceptions.ValidationException
 
-import java.util.List;
-
-public class MinItemsValidator implements KeywordValidator {
-    @Override
-    public @Nullable PathToSchemasMap validate(
-        ValidationData data
-    ) throws ValidationException {
-        var minItems = data.schema().minItems;
-        if (minItems == null) {
-            return null;
+class MinItemsValidator : KeywordValidator {
+    @Throws(ValidationException::class)
+    override fun validate(
+        data: ValidationData
+    ): PathToSchemasMap? {
+        val minItems: Int = data.schema.minItems ?: return null
+        if (data.arg !is List<*>) {
+            return null
         }
-        if (!(data.arg() instanceof List<?> listArg)) {
-            return null;
+        if (data.arg.size < minItems) {
+            throw ValidationException("Value ${data.arg} is invalid because has < the minItems of $minItems")
         }
-        if (listArg.size() < minItems) {
-            throw new ValidationException("Value " + listArg + " is invalid because has < the minItems of " + minItems);
-        }
-        return null;
+        return null
     }
 }
