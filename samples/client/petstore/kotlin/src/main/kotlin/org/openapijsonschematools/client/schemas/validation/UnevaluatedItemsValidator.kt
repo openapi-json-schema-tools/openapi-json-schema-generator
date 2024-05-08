e21dd3403e4990d46b30b8b558c6c1357b4e6a92
Justin Black <justin.a.black@gmail.com>
2024-05-08 16:53:23 -0700

    Updates UnevaluatedItemsValidator + UnevaluatedPropertiesValidator

--- samples/client/petstore/kotlin/src/main/kotlin/org/openapijsonschematools/client/schemas/validation/UnevaluatedItemsValidator.kt
+++ samples/client/petstore/kotlin/src/main/kotlin/org/openapijsonschematools/client/schemas/validation/UnevaluatedItemsValidator.kt
@@ -1,52 +1,41 @@
-package org.openapijsonschematools.client.schemas.validation;
+package org.openapijsonschematools.client.schemas.validation
 
-import org.checkerframework.checker.nullness.qual.Nullable;
-import org.openapijsonschematools.client.exceptions.ValidationException;
+import org.openapijsonschematools.client.exceptions.ValidationException
 
-import java.util.ArrayList;
-import java.util.List;
-
-public class UnevaluatedItemsValidator implements KeywordValidator {
-    @Override
-    public @Nullable PathToSchemasMap validate(
-        ValidationData data
-    ) throws ValidationException {
-        var unevaluatedItems = data.schema().unevaluatedItems;
-        if (unevaluatedItems == null) {
-            return null;
+class UnevaluatedItemsValidator : KeywordValidator {
+    @Throws(ValidationException::class)
+    override fun validate(
+        data: ValidationData
+    ): PathToSchemasMap? {
+        val unevaluatedItems: Class<out JsonSchema<*>> = data.schema.unevaluatedItems ?: return null
+        val knownPathToSchemas: PathToSchemasMap = data.knownPathToSchemas ?: return null
+        if (data.arg !is List<*>) {
+            return null
         }
-        var knownPathToSchemas = data.knownPathToSchemas();
-        if (knownPathToSchemas == null) {
-            return null;
+        if (data.arg.isEmpty()) {
+            return null
         }
-        if (!(data.arg() instanceof List<?> listArg)) {
-            return null;
-        }
-        if (listArg.isEmpty()) {
-            return null;
-        }
-        PathToSchemasMap pathToSchemas = new PathToSchemasMap();
-        int minIndex = data.schema().prefixItems != null ? data.schema().prefixItems.size() : 0;
-        JsonSchema<?> unevaluatedItemsSchema = JsonSchemaFactory.getInstance(unevaluatedItems);
-        for(int i = minIndex; i < listArg.size(); i++) {
-            List<Object> itemPathToItem = new ArrayList<>(data.validationMetadata().pathToItem());
-            itemPathToItem.add(i);
+        val pathToSchemas = PathToSchemasMap()
+        val minIndex = if (data.schema.prefixItems != null) data.schema.prefixItems.size else 0
+        val unevaluatedItemsSchema = JsonSchemaFactory.getInstance(unevaluatedItems)
+        for (i in minIndex until data.arg.size) {
+            val itemPathToItem: List<Any> = data.validationMetadata.pathToItem + i
             if (knownPathToSchemas.containsKey(itemPathToItem)) {
-                continue;
+                continue
             }
-            ValidationMetadata itemValidationMetadata = new ValidationMetadata(
-                    itemPathToItem,
-                    data.validationMetadata().configuration(),
-                    data.validationMetadata().validatedPathToSchemas(),
-                    data.validationMetadata().seenClasses()
-            );
+            val itemValidationMetadata = ValidationMetadata(
+                itemPathToItem,
+                data.validationMetadata.configuration,
+                data.validationMetadata.validatedPathToSchemas,
+                data.validationMetadata.seenClasses
+            )
             if (itemValidationMetadata.validationRanEarlier(unevaluatedItemsSchema)) {
                 // todo add_deeper_validated_schemas
-                continue;
+                continue
             }
-            PathToSchemasMap otherPathToSchemas = JsonSchema.validate(unevaluatedItemsSchema, listArg.get(i), itemValidationMetadata);
-            pathToSchemas.update(otherPathToSchemas);
+            val otherPathToSchemas = JsonSchema.validate(unevaluatedItemsSchema, data.arg[i], itemValidationMetadata)
+            pathToSchemas.update(otherPathToSchemas)
         }
-        return pathToSchemas;
+        return pathToSchemas
     }
 }
--- samples/client/petstore/kotlin/src/main/kotlin/org/openapijsonschematools/client/schemas/validation/UnevaluatedPropertiesValidator.kt
+++ samples/client/petstore/kotlin/src/main/kotlin/org/openapijsonschematools/client/schemas/validation/UnevaluatedPropertiesValidator.kt
@@ -1,49 +1,36 @@
-package org.openapijsonschematools.client.schemas.validation;
+package org.openapijsonschematools.client.schemas.validation
 
-import org.checkerframework.checker.nullness.qual.Nullable;
-import org.openapijsonschematools.client.exceptions.ValidationException;
+import org.openapijsonschematools.client.exceptions.ValidationException
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-public class UnevaluatedPropertiesValidator implements KeywordValidator {
-    @Override
-    public @Nullable PathToSchemasMap validate(
-        ValidationData data
-    ) throws ValidationException {
-        var unevaluatedProperties = data.schema().unevaluatedProperties;
-        if (unevaluatedProperties == null) {
-            return null;
+class UnevaluatedPropertiesValidator : KeywordValidator {
+    @Throws(ValidationException::class)
+    override fun validate(
+        data: ValidationData
+    ): PathToSchemasMap? {
+        val unevaluatedProperties: Class<out JsonSchema<*>> = data.schema.unevaluatedProperties ?: return null
+        val knownPathToSchemas: PathToSchemasMap = data.knownPathToSchemas ?: return null
+        if (data.arg !is Map<*, *>) {
+            return null
         }
-        var knownPathToSchemas = data.knownPathToSchemas();
-        if (knownPathToSchemas == null) {
-            return null;
-        }
-        if (!(data.arg() instanceof Map<?, ?> mapArg)) {
-            return null;
-        }
-        PathToSchemasMap pathToSchemas = new PathToSchemasMap();
-        JsonSchema<?> unevaluatedPropertiesSchema = JsonSchemaFactory.getInstance(unevaluatedProperties);
-        for(Map.Entry<?, ?> entry: mapArg.entrySet()) {
-            if (!(entry.getKey() instanceof String propName)) {
-                throw new ValidationException("Map keys must be strings");
+        val pathToSchemas = PathToSchemasMap()
+        val unevaluatedPropertiesSchema = JsonSchemaFactory.getInstance(unevaluatedProperties)
+        for ((key, value) in data.arg.entries) {
+            if (key !is String) {
+                throw ValidationException("Map keys must be strings")
             }
-            List<Object> propPathToItem = new ArrayList<>(data.validationMetadata().pathToItem());
-            propPathToItem.add(propName);
+            val propPathToItem: List<Any> = data.validationMetadata.pathToItem + key
             if (knownPathToSchemas.containsKey(propPathToItem)) {
-                continue;
+                continue
             }
-            @Nullable Object propValue = mapArg.get(propName);
-            ValidationMetadata propValidationMetadata = new ValidationMetadata(
-                    propPathToItem,
-                    data.validationMetadata().configuration(),
-                    data.validationMetadata().validatedPathToSchemas(),
-                    data.validationMetadata().seenClasses()
-            );
-            PathToSchemasMap otherPathToSchemas = JsonSchema.validate(unevaluatedPropertiesSchema, propValue, propValidationMetadata);
-            pathToSchemas.update(otherPathToSchemas);
+            val propValidationMetadata = ValidationMetadata(
+                propPathToItem,
+                data.validationMetadata.configuration,
+                data.validationMetadata.validatedPathToSchemas,
+                data.validationMetadata.seenClasses
+            )
+            val otherPathToSchemas = JsonSchema.validate(unevaluatedPropertiesSchema, value, propValidationMetadata)
+            pathToSchemas.update(otherPathToSchemas)
         }
-        return pathToSchemas;
+        return pathToSchemas
     }
 }
