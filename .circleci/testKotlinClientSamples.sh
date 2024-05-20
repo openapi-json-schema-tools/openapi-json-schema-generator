ec334a61990d359633e5f7f4239d0f1602531a49
Justin Black <justin.a.black@gmail.com>
2024-05-19 17:20:06 -0700

    Removes nodaemon arg on gradle test invocation

--- .circleci/testKotlinClientSamples.sh
+++ .circleci/testKotlinClientSamples.sh
@@ -1,2 +1,2 @@
-  (cd samples/client/3_0_3_unit_test/kotlin && gradle wrapper && ./gradlew cleanTest test --no-daemon)
-  (cd samples/client/3_1_0_unit_test/kotlin && gradle wrapper && ./gradlew cleanTest test --no-daemon)
+  (cd samples/client/3_0_3_unit_test/kotlin && gradle wrapper && ./gradlew cleanTest test)
+  (cd samples/client/3_1_0_unit_test/kotlin && gradle wrapper && ./gradlew cleanTest test)
